e8e3001e5c01e3eacd950e68c83cf13c24293f4b
MK An <marinear212@hotmail.com>
2017-07-25 18:06:01 +0000

    webdev

---
+++ practice/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Enter your first name.");
+var lastName = prompt("Enter your last name.");
+var age = prompt("Enter your age.");
+
+console.log("Nice to meet you, " + firstName + " " + lastName);
+console.log("You are " + age + " years old!");
